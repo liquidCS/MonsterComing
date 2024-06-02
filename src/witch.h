0043a2138839870761f77ec612424c11bfe35ff7
Carolll39 <163887258+Carolll39@users.noreply.github.com>
2024-06-02 17:00:09 +0800

    Update witch.h

--- src/witch.h
+++ src/witch.h
@@ -1,6 +1,14 @@
 #ifndef WITCH_H
 #define WITCH_H
 
+#include "raylib.h"
+#include "zombies.h"
+
+#define NORMAL_ATTACK_WIDTH 100
+#define NORMAL_ATTACK_HEIGHT 100
+
+#define ANIMATION_TIME 2
+
 void initWitchAttack();
 
 void witchNormalAttack();
